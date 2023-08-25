8d4d43017aa56314b059d28cde793953bf446784
BauTTTTT <127962360+BauTTTTT@users.noreply.github.com>
2023-08-24 23:21:32 -0300

    Add files via upload

---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,15 @@
+import "./nAVbAR.css"
+
+export default function NavBar() {
+    return(
+        <div class="navbar">
+    <a href="#home">Inicio</a>
+    <a href="#news">Noticias</a>
+    <a href="#about">Acerca de</a>
+    <a href="#services">Servicios</a>
+    <a href="#contact">Contacto</a>
+</div>
+    )
+}
+
+
